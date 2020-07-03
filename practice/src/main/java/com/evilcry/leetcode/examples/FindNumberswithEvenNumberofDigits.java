cfa8e8dbfbb3702d29da722f0459f6b24c4f26f2
surick <jk103@qq.com>
2020-07-03 08:54:42 +0800

    practice leetcode

--- practice/src/main/java/com/evilcry/leetcode/examples/FindNumberswithEvenNumberofDigits.java
+++ practice/src/main/java/com/evilcry/leetcode/examples/FindNumberswithEvenNumberofDigits.java
@@ -49,4 +49,21 @@ public class FindNumberswithEvenNumberofDigits {
 
         // return (int) Arrays.stream(nums).filter(num -> String.valueOf(num).length() % 2 == 0).count();
     }
+
+
+    public int findNumbers1(int[] nums) {
+        int sol = 0;                            // Solution variable initialised to 0
+        for(int n : nums)                       // Iterate over each number
+            if(getNumberOfDigits(n) % 2 == 0)   // Call getNumberOfDigits which return the total number of digits in the number
+                sol++;                          // if even number of digits are returned we increment sol by 1
+        return sol;
+    }
+
+    public int getNumberOfDigits(int num) {
+        int count = 1;                          // Initialise count to 1, not 0 cause we are dividing num/10 in while loop
+        while((num /= 10) != 0)                 // Iterate until number is 0
+            count++;                            // Increment count until number is 0
+        return count;
+    }
+
 }
